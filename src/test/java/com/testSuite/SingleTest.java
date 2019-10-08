ba0f09a7ff3e4b427bc9271681398eeea70fcbee
gokool4 <gokool4@yahoo.com>
2019-10-08 11:38:17 +0100

    Add files via upload

---
+++ src/test/java/com/testSuite/SingleTest.java
@@ -0,0 +1,22 @@
+package com.testSuite;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class SingleTest extends BrowserStackTestNGTest {
+
+    @Test
+    public void test() throws Exception {
+        driver.get("https://www.browserstack.com/");
+        String headerText = driver.findElement(By.id("primary-menu")).getText();
+        Assert.assertTrue(headerText.contains("Sign in"));
+        Assert.assertFalse(headerText.contains("Account"));
+
+        WebElement RightDevicesLink  = driver.findElement(By.id("signupModalButton"));
+        RightDevicesLink.click();
+
+        Assert.assertEquals(driver.getTitle(),"Get Started For Free & Access 2000+ Mobile Devices & Browsers | BrowserStack");
+    }
+}
